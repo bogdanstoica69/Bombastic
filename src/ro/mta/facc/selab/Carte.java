b8766799fe9fb4e53e410cc3f45cff64d346d590
bogdan <f.alexandru13@yahoo.com>
2020-10-23 13:11:56 +0300

    Modificare an aparitie
    Signed-off-by: bogdan <f.alexandru13@yahoo.com>

--- src/ro/mta/facc/selab/Carte.java
+++ src/ro/mta/facc/selab/Carte.java
@@ -3,7 +3,7 @@ package ro.mta.facc.selab;
 public class Carte implements Comparable<Carte> {
     private String titlu;
     private String autor;
-    private int an_aparitie;
+    private int an_publicatie;
     private int nrPagini;
 
     @Override
@@ -20,13 +20,13 @@ public class Carte implements Comparable<Carte> {
       // return Integer.compare(this.nrPagini, o.nrPagini);
     }
 
-    public Carte(String titlu, String autor, int an_aparitie, int nrPagini) {
+    public Carte(String titlu, String autor, int an_publicatie, int nrPagini) {
         this.titlu = titlu;
         this.autor = autor;
-        this.an_aparitie = an_aparitie;
+        this.an_publicatie = an_publicatie;
         this.nrPagini = nrPagini;
     }
 
     @Override
     public String toString() {
-        return this.titlu + " " + this.autor + " aparuta in anul " + this.an_aparitie ;}}
+        return this.titlu + " " + this.autor + " aparuta in anul " + this.an_publicatie;}}
